33b7e7fd465f5480f1f0689b1104a4ce289c3c63
Theriley106 <christopherlambert106@gmail.com>
2018-09-07 20:27:12 -0700

    Secret codes are impelmented into the web app

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request, url_for, redirect, Markup, js
 
 app = Flask(__name__, static_url_path='/static')
 
+SECRETS = open("secrets.txt").read().split("\n")
+APP_ID = SECRETS[0]
+APP_CODE = SECRETS[1]
 
 @app.route('/', methods=['GET'])
 def index():
-	return render_template("index.html")
+	return render_template("index.html", appid=APP_ID, appcode=APP_CODE)
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000)
