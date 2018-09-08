971002cc9ddeb38b2c1d464cc692935ea5083ce5
Theriley106 <christopherlambert106@gmail.com>
2018-09-08 13:26:29 -0700

    Fixed notifications

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 def send_notication(message):
-	res = requests.post("http://127.0.0.1:5000/notification?message=Test&patient=213")
+	res = requests.post("https://my-doctor-alexa.herokuapp.com/notification?message=Test&patient=213")
 
 if __name__ == '__main__':
 	send_notication("TESTING")
