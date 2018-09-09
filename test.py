9f80a23f021234e15c14de0ad96f117791e43ec0
Theriley106 <christopherlambert106@gmail.com>
2018-09-09 09:22:38 -0700

    Removed test program

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def send_notication(message):
-	res = requests.post("https://my-doctor-alexa.herokuapp.com/notification?message=Test&patient=213")
-
-if __name__ == '__main__':
-	send_notication("TESTING")
